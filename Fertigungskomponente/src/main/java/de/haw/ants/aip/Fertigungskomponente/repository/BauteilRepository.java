d5c05b64e81721aa56b097f100cff104716ec599
TimSielemann <TimSielemann@googlemail.com>
2015-06-08 19:38:13 +0200

    Bauteilkomponente

--- Fertigungskomponente/src/main/java/de/haw/ants/aip/Fertigungskomponente/repository/BauteilRepository.java
+++ Fertigungskomponente/src/main/java/de/haw/ants/aip/Fertigungskomponente/repository/BauteilRepository.java
@@ -1,9 +1,10 @@
 package de.haw.ants.aip.Fertigungskomponente.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Component;
 
 import de.haw.ants.aip.Fertigungskomponente.entity.Bauteil;
-
+@Component
 public interface BauteilRepository extends
 		PagingAndSortingRepository<Bauteil, Long> {
 
